6e519f6fa3bf877b07c3ff57f95ce6b6e5805381
Alexei_korn <aykornjushenkov@gmail.com>
2021-11-01 21:51:43 +0300

    2. Каркас шахматной доски [#792] Отработка замечаний Ci Bot

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -31,7 +31,7 @@ public class BishopBlack implements Figure {
         int size = Math.abs(deltaX);
         Cell[] steps = new Cell[size];
         for (int index = 0; index < size; index++) {
-            steps[index] = Cell.findBy(deltaX < 0 ? --x : ++x, deltaY < 0 ? --y : ++y);
+            steps[index] = Cell.findBy(deltaX < 0 ? -- x : ++ x, deltaY < 0 ? -- y : ++ y);
         }
         return steps;
     }
